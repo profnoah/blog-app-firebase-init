f0b5954f935de89f66d81e8299fcf80b0dabd739
profnoah <noah@clarusway.com>
2021-09-30 22:53:37 +0300

    v7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
 import AuthContextProvider from "./context/AuthContextProvider";
+import AppRouter from "./router/AppRouter";
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
-        <Navbar />
+        <AppRouter />
       </AuthContextProvider>
     </div>
   );
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const AppRouter = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default AppRouter;
