cbc22be78d9b61beebb890292d00da77642eb584
profnoah <noah@clarusway.com>
2021-09-30 21:41:55 +0300

    v4

--- src/context/AuthContextProvider.js
+++ src/context/AuthContextProvider.js
@@ -1,14 +1,44 @@
 import React, { useContext, createContext } from "react";
+import { auth, googleProvider } from "../utils/firebaseUtil";
 
 //! Create context for autentication data
 const AuthContext = createContext();
 
 //! Define a function to get data from Auth context
-function useAuth() {
+export function useAuth() {
   return useContext(AuthContext);
 }
 
 const AuthContextProvider = () => {
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    auth.signOut();
+  }
+
+  function loginWithGoogle() {
+    googleProvider.setCustomParameters({ prompt: "select_account" });
+    auth.signInWithPopup(googleProvider);
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
+
   return <div></div>;
 };
 
