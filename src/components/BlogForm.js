21163aa41ad94584df3ac16e4427f2ad465c9cd4
profnoah <noah@clarusway.com>
2021-10-05 22:50:44 +0300

    v19

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -30,7 +30,66 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const BlogForm = (props) => {
-  return <div></div>;
+  const { blog, handler } = props;
+  const classes = useStyles();
+  const [newBlog, setNewBlog] = useState(blog);
+
+  return (
+    <form className={classes.form} onSubmit={() => handler(newBlog)}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            fullWidth
+            required
+            id="title"
+            label="Title"
+            name="title"
+            value={newBlog.title}
+            autoFocus
+            onChange={(e) => setNewBlog({ ...newBlog, title: e.target.value })}
+          />
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          variant="outlined"
+          required
+          fullWidth
+          name="image"
+          label="Image URL"
+          type="text"
+          id="image"
+          value={newBlog.image}
+          onChange={(e) => setNewBlog({ ...newBlog, image: e.target.value })}
+        />
+      </Grid>
+
+      <Grid item xs={12}>
+        <TextField
+          id="outlined-multiline-static"
+          label="Content"
+          name="content"
+          multiline
+          required
+          value={newBlog.content}
+          onChange={(e) => setNewBlog({ ...newBlog, content: e.target.value })}
+          fullWidth
+          rows={15}
+          variant="outlined"
+        />
+      </Grid>
+
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        className={classes.submit}
+      >
+        Submit
+      </Button>
+    </form>
+  );
 };
 
 export default BlogForm;
