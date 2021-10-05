0db2183ee8c297b38c5d599b7b8e91c9a903e3d4
profnoah <noah@clarusway.com>
2021-10-05 17:05:17 +0300

    v17

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,9 @@
-import React, { createContext } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
